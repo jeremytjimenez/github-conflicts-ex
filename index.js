fc5b76b93952f8598b55be490a1a7bc6a5b9a8c7
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-19 13:03:40 -0400

    made changes to addPersonAsFriend

---
+++ index.js
@@ -0,0 +1,17 @@
+function createPerson(name, age) {
+    return {
+      name,
+      age,
+      friends: [],
+    };
+  }
+  
+function addPersonAsFriend(person, friend) {
+    person.friends.push(friend);
+    return person;
+}
+
+module.exports = {
+    createPerson,
+    addPersonAsFriend,
+};
